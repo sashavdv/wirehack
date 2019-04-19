b5533185d012c1990ec10df3672dbe4485e938a7
Robin <robin.machiels@gmail.com>
2019-04-20 01:31:33 +0200

    added directories loans and investments

---
+++ src/pages/investments/createLoan.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <el-form>
+            <el-input placeholder="Enter amount..."></el-input>
+            <el-select v-model="value" placeholder="Select">
+                <el-option
+                        v-for="item in options"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                </el-option>
+            </el-select>
+        </el-form>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "create-loan"
+    }
+</script>
+
+<style scoped>
+
+</style>
