31d58711d9441c0a1e1716ca566c4678062ac64c
Nick Castermans <>
2019-04-20 02:35:19 +0200

    header add loans and profile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,15 +8,30 @@
       </el-col>
       <el-col :span="6">
         <div class="grid-content">
-            <el-dropdown @command="handleCommand">
+            <el-dropdown @command="Profile">
               <el-button type="primary" plain  size="small">
                 Profile<i class="el-icon-arrow-down el-icon--right"></i>
               </el-button>
               <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item command="profile">Profile</el-dropdown-item>
-                <el-dropdown-item command="loans">Loans</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
+          <el-dropdown @command="Loans">
+            <el-button type="primary" plain  size="small">
+              Loans<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="loans">Loans</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <el-dropdown @command="Invest">
+            <el-button type="primary" plain  size="small">
+              Invest<i class="el-icon-arrow-down el-icon--right"></i>
+            </el-button>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="loans">Loans</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
         </div>
       </el-col>
     </el-row>
@@ -27,13 +42,24 @@
 export default {
   name: 'Header',
   methods: {
-    handleCommand (command) {
+    Profile (command) {
+      switch (command) {
+        case 'profile':
+          this.$router.push('/Profile')
+          break
+      }
+    },
+    Loans (command) {
       switch (command) {
         case 'loans':
           this.$router.push('/Loans')
           break
-        case 'profile':
-          this.$router.push('/Profile')
+      }
+    },
+    Invest (command) {
+      switch (command) {
+        case 'invest':
+          this.$router.push('/Invest')
           break
       }
     }
