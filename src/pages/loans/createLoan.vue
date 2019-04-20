fa914024d2c023aad1169bfc53f624a340d04879
Robin <robin.machiels@gmail.com>
2019-04-20 03:20:30 +0200

    createloan structuur is klaar

--- src/pages/loans/createLoan.vue
+++ src/pages/loans/createLoan.vue
@@ -1,19 +1,27 @@
 <template>
     <div>
         <el-form>
-            Amount to borrow: <el-input placeholder="Enter amount..."></el-input>
-            Months: <el-input-number v-model="months" :min="3" :max="36"></el-input-number>
-            Goal: <el-select v-model="goals" placeholder="Select">
-                <el-option
-                        v-for="item in options"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value">
-                </el-option>
-            </el-select>
-            I want to be contacted by investors: <el-checkbox v-model="contact_me">Option</el-checkbox>
+            <el-form-item label="Amount to borrow:">
+                <el-input v-model="form.amount" placeholder="Enter amount..."></el-input>
+            </el-form-item>
+            <el-form-item label="Months:">
+                <el-input-number v-model="form.months" :min="3" :max="36"></el-input-number>
+            </el-form-item>
+            <el-form-item label="Goal:">
+                <el-select v-model="form.goal" placeholder="Select">
+                    <el-option label="Car" value="car"></el-option>
+                    <el-option label="Startup" value="startup"></el-option>
+                    <el-option label="Reason3" value="reason3"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="I want to be contacted by investors:">
+                <el-checkbox v-model="form.contact_me"></el-checkbox>
+            </el-form-item>
+            <el-form-item label="Description (optional)">
+                <el-input type="textarea" v-model="form.description"></el-input>
+            </el-form-item>
+            <el-button type="primary" @click="onSubmit">Create</el-button>
         </el-form>
-
     </div>
 </template>
 
@@ -22,21 +30,28 @@ export default {
   name: 'create-loan',
   data () {
     return {
-      options: [{
-        value: 'car',
-        label: 'Car'
+      form: {
+        amount: '',
+        months: '3',
+        goal: '',
+        contact_me: false,
+        description: ''
       },
-      {
-        value: 'startup',
-        label: 'Startup'
-      },
-      {
-        value: 'reason3',
-        label: 'Reason3'
+      methods: {
+        onSubmit () {
+          const axios = require('axios')
+          axios.post('loaners/loans', {
+            form: this.form
+          })
+            .then((response) => {
+              console.log(this.notifications)
+            })
+            .catch(function (error) {
+              // handle error
+              console.log(error)
+            })
+        }
       }
-      ],
-      value: '',
-      contact_me: true
     }
   }
 }
