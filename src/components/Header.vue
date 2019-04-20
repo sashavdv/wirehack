4a2ae7fabe0fb9c316fb4a31be3812516bc1c58e
Nick Castermans <>
2019-04-20 11:53:01 +0200

    header fixed for roles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
     <el-row type="flex" class="row-bg" justify="space-between">
-      <el-col :span="6">
+      <el-col :span="6" :sm="10" :xs="12" :md="8" :lg="6">
         <div class="grid-content logo">
-          <router-link :to="{ path: '/' }" class="px-3 py-3 no-underline text-black bg-primary rounded-bg">Home</router-link>
-          <router-link v-show="isLoaner()" :to="{ path: 'LoanCreate' }" class="px-3 py-3 no-underline text-black bg-primary rounded-bg">Create loan</router-link>
-          <router-link v-show="isInvestor()" :to="{ path: 'Invest' }" class="px-3 py-3 no-underline text-black bg-primary rounded-bg">Create investment</router-link>
+          <router-link :to="{ path: '/' }" class="px-3 py-3 no-underline text-black rounded-lg">Home</router-link>
+          <router-link v-show="isLoaner()" :to="{ path: 'LoanCreate' }" class="px-3 py-3 no-underline text-black rounded-lg">Create loan</router-link>
+          <router-link v-show="isInvestor()" :to="{ path: 'Invest' }" class="px-3 py-3 no-underline text-black rounded-lg">Create investment</router-link>
         </div>
       </el-col>
-      <el-col :span="6">
+      <el-col :span="6" :sm="10" :xs="12" :md="8" :lg="6">
         <div class="grid-content">
-          <el-button class="px-3 py-3 bg-primary text-black rounded-bg" v-show="!user" type="primary" plain size="small" @click="openLoginDialog">Login</el-button>
+          <el-button class="px-3 py-3 text-black rounded-lg" v-show="!user" type="primary" plain size="small" @click="openLoginDialog">Login</el-button>
 
           <el-dropdown v-show="user" @command="Profile">
               <el-button type="primary" plain  size="small">
                 Profile<i class="el-icon-arrow-down el-icon--right"></i>
               </el-button>
               <el-dropdown-menu slot="dropdown" class="bg-primary">
-                <el-dropdown-item class="bg-primary" command="profile">Profile</el-dropdown-item>
-                <el-dropdown-item class="bg-primary" v-show="isLoaner()" command="loans">Loans</el-dropdown-item>
-                <el-dropdown-item class="bg-primary" v-show="isInvestor()" command="loans">Investor</el-dropdown-item>
-                <el-dropdown-item class="bg-primary" command="logout">Logout</el-dropdown-item>
+                <el-dropdown-item class="bg-primary text-white" command="profile">Profile</el-dropdown-item>
+                <el-dropdown-item class="bg-primary text-white" v-show="isLoaner()" command="loans">Loans</el-dropdown-item>
+                <el-dropdown-item class="bg-primary text-white" v-show="isInvestor()" command="loans">Investor</el-dropdown-item>
+                <el-dropdown-item class="bg-primary text-white" command="logout">Logout</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
         </div>
@@ -62,8 +62,8 @@ export default {
       this.$refs.loginDialog.toggleVisibility()
     },
     isInvestor () {
-      if (this.$store.getters.getUser) {
-        if (this.$store.getters.getUser.type === 'investor') {
+      if (this.user) {
+        if (this.user.type === 'investor') {
           console.log('user is investor')
           return true
         } else {
@@ -76,8 +76,8 @@ export default {
       }
     },
     isLoaner () {
-      if (this.$store.getters.getUser) {
-        if (this.$store.getters.getUser.type === 'loaner') {
+      if (this.user) {
+        if (this.user.type === 'loaner') {
           console.log('user is loaner')
           return true
         } else {
--- src/pages/homepage.vue
+++ src/pages/homepage.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1 class="my-24 text-center">Home</h1>
     <el-row>
       <el-col :xs="24" :sm="12" :md="12" :lg="12" class="text-center">
         <el-button type="primary" class="rounded-lg px-24 text-xl" @click="borrow">Borrow</el-button>
